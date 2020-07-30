34cf53c39991fdcf786f7c1f7f08e1479be79cb7
Skylar Calvin <rducky01@gmail.com>
2020-07-30 03:16:09 -0500

    added static directory

--- mainSite/settings.py
+++ mainSite/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_root/'
 
 STATICFILES_DIRS = [
     '/var/www/calvin-tech/static/',
