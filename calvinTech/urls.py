71245ce59f7610fee854385ab43a9442d9164e6b
Skylar Calvin <skylarcalvin5@gmail.com>
2020-08-21 04:30:19 -0500

    Added sentiment analyzer

--- calvinTech/urls.py
+++ calvinTech/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('DS8660_Module4', views.DS8660_Module4, name = 'DS8660_Module4')
+    path('DS8660_Module4', views.DS8660_Module4, name = 'DS8660_Module4'),
+    path('Sentiment_Analyzer', views.Sentiment_Analyzer, name = 'Sentiment_Analyzer')
 ]
--- calvinTech/views.py
+++ calvinTech/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .getSentiment import getSentimentForm, getSentiment
+import pandas as pd
+import numpy as np
 
 # Create your views here.
 def index(request):
@@ -8,3 +11,28 @@ def index(request):
 def DS8660_Module4(request):
     response = render(request, 'DS8660_Module4.html')
     return response
+
+def Sentiment_Analyzer(request):
+
+    # if this is a POST request we need to process the form data.
+    if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request:
+        form = getSentimentForm(request.POST)
+
+        # Check whether it's valid.
+        if form.is_valid():
+
+            # Put the user imput into a dataframe and pull sentiment.
+            data = pd.DataFrame([request.POST['message']], columns=['input'])
+            sentiment = getSentiment(data)
+            
+            output = 'You have submitted a ' + sentiment['sentiment'][0] + ' sentence or paragragh.'
+
+
+    else: 
+        
+        form = getSentimentForm()
+        output = "You haven't submitted anything for analysis."
+
+    return render(request, 'Sentiment_Analyzer.html', {'form': form, 'output': output})
