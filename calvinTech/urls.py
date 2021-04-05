51755754f37755b12e39c5a34ede5c013cf78b3c
root <root@srv-skyhelm-web01.unassigned-domain>
2021-04-05 12:33:35 +0000

    Corrected settings.py

--- calvinTech/urls.py
+++ calvinTech/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
@@ -7,3 +7,5 @@ urlpatterns = [
     path('DS8660_Module4', views.DS8660_Module4, name = 'DS8660_Module4'),
     path('Sentiment_Analyzer', views.Sentiment_Analyzer, name = 'Sentiment_Analyzer')
 ]
+
+urlpatterns += staticfiles_urlpatterns()
--- mainSite/settings.py
+++ mainSite/settings.py
@@ -27,6 +27,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
+    '192.168.10.53',
     'calvin-tech.net',
     'www.calvin-tech.net',
 ]
